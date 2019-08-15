b69efa7c999fbe1c35970e2fc82df1de57b46964
Michael Williams <manicdesigner@gmail.com>
2019-08-15 09:44:32 -0400

    add card, image, cardInfo, and name to function GithubCard

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,14 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+axios
+  .get('https://api.github.com/users/mikeyjwilliams')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -25,6 +32,24 @@
 */
 
 const followersArray = [];
+
+function GithubCard(githubInfo) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  const image = document.createElement('img');
+  image.src = githubInfo.avatar_url;
+  card.appendChild(image);
+
+  const cardInfo = document.createElement('div');
+  cardInfo.classList.add('card-info');
+  card.appendChild(cardInfo);
+
+  const name = document.createElement('h3');
+  name.classList.add('name');
+  name.textContent = githubInfo.name;
+  card.appendChild(name);
+}
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
